e4291f155e1d5eeb4ecae54f5d034c3dd0aa8cca
Alfred Boateng <programmingwithalfred@gmail.com>
2022-11-14 12:51:31 +0000

    Devise auth setup conplete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  layout :set_layout
 
   protected
 
@@ -9,4 +10,13 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name])
     devise_parameter_sanitizer.permit(:account_update, keys:  %i[name])
   end
+
+  def set_layout
+  if current_user
+    "application"
+  else
+    "auth"
+  end
+end
+
 end
