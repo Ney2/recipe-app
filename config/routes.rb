01de90f0ddcaaff34d2e4a2ee8696e812f37726b
Alfred Boateng <programmingwithalfred@gmail.com>
2022-11-14 14:42:43 +0000

    Setup food recipe

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
+  resources :recipe_foods
   resources :foods
   resources :recipes
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
   root "home#index"
 end
