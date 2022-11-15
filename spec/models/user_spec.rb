3f48766bd0321beb17a1d88acb88b2cd18c9c9c9
Ney2 <kidistdinku5057@gmail.com>
2022-11-15 20:11:44 +0300

    Add model test for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # test associations
+  describe 'associations' do
+    it 'has many recipes' do
+      association = described_class.reflect_on_association(:recipes)
+      expect(association.macro).to eq :has_many
+    end
+
+    it 'has many foods' do
+      association = described_class.reflect_on_association(:foods)
+      expect(association.macro).to eq :has_many
+    end
+  end
+
+  # test validations
+
+  describe 'validations' do
+    it 'is not valid without a name' do
+      user = User.new(name: nil)
+      expect(user).to_not be_valid
+    end
+  end
 end
