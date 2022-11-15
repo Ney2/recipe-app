c226ce69fef363322879c4006a79c1ee846847c0
Alfred Boateng <programmingwithalfred@gmail.com>
2022-11-15 14:18:07 +0000

    Load all associated food in recipe details page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resources :recipe_foods, except: %i[update]
-  resources :foods, except: %i[update]
-  resources :recipes, except: %i[update]
-  #render public recipes
+  resources :recipe_foods, except: %i[update edit]
+  resources :foods, except: %i[update edit]
+   resources :recipes do
+    resources :recipe_foods, except: [:edit, :update]
+  end
   get 'public_recipes', to: 'recipes#public_recipes'
   devise_for :users
   root "home#index"
