2d4ca0bb14d9857b68287bf3694c2643eccc12ca
Alfred Boateng <programmingwithalfred@gmail.com>
2022-11-15 14:35:49 +0000

    Update ui add general shopping list

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   resources :recipe_foods, except: %i[update edit]
   resources :foods, except: %i[update edit]
-   resources :recipes do
+  resources :recipes do
     resources :recipe_foods, except: [:edit, :update]
+    resources :general_shopping_lists, only: [:index]
   end
   get 'public_recipes', to: 'recipes#public_recipes'
   devise_for :users
